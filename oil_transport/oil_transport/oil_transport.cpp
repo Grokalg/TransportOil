2ea734cdeaa3d4694a98d43833ac3286bd67751e
Grokalg <98714600+Grokalg@users.noreply.github.com>
2022-09-26 19:03:41 +0300

    Предфинальная версия
    Нет обработки корректности ввода пользователя

--- oil_transport/oil_transport/oil_transport.cpp
+++ oil_transport/oil_transport/oil_transport.cpp
@@ -26,48 +26,114 @@ struct cs
 
 void CreateTube(tube& new_tube)
 {
-	double length, diameter;
-	cout << "Введите длину: ";
-	cin >> length;
-	if (length <= 0)
+	if (new_tube.created == true)
 	{
-		cout << "Длина трубы не может быть отрицательной или 0. Введите данные повторно:\n";
+		int user_choice;
+		cout << "Вы точно хотите перезаписать трубу?\n";
+		cout << "Выберете: <0>, если не хотите перезаписывать трубу,\n";
+		cout << "          <1>, если хотите перезаписать\n";
+		cin >> user_choice;
+		if (user_choice == 1)
+		{
+			double length, diameter;
+			cout << "Введите длину: ";
+			cin >> length;
+			if (length <= 0)
+			{
+				cout << "Длина трубы не может быть отрицательной или 0. Введите данные повторно:\n";
+				cin >> length;
+			}
+			cout << "Введите диаметр: ";
+			cin >> diameter;
+			if (diameter == 0)
+			{
+				cout << "Диаметр трубы не может составлять 0 м. Введите данные повторно:\n";
+				cin >> diameter;
+			}
+			cout << "Труба исправна\n\n";
+			new_tube.length = length;
+			new_tube.diameter = diameter;
+			new_tube.condition = "Труба исправна";
+			new_tube.saved = false;
+			new_tube.created = true;
+		}
+	}
+	else
+	{
+		double length, diameter;
+		cout << "Введите длину: ";
 		cin >> length;
-	}
-	cout << "Введите диаметр: ";
-	cin >> diameter;
-	if (diameter == 0)
-	{
-		cout << "Диаметр трубы не может составлять 0 м. Введите данные повторно:\n";
+		if (length <= 0)
+		{
+			cout << "Длина трубы не может быть отрицательной или 0. Введите данные повторно:\n";
+			cin >> length;
+		}
+		cout << "Введите диаметр: ";
 		cin >> diameter;
+		if (diameter == 0)
+		{
+			cout << "Диаметр трубы не может составлять 0 м. Введите данные повторно:\n";
+			cin >> diameter;
+		}
+		cout << "Труба исправна\n\n";
+		new_tube.length = length;
+		new_tube.diameter = diameter;
+		new_tube.condition = "Труба исправна";
+		new_tube.saved = false;
+		new_tube.created = true;
 	}
-	cout << "Труба исправна\n\n";
-	new_tube.length = length;
-	new_tube.diameter = diameter;
-	new_tube.condition = "Труба исправна";
-	new_tube.saved = false;
-	new_tube.created = true;
 }
 
 void CreateStation(cs& new_cs)
 {
-	int rooms, active_rooms, efficiency;
-	cout << "Укажите количество цехов: ";
-	cin >> rooms;
-	cout << "Укажите количество работающих цехов: ";
-	cin >> active_rooms;
-	if (active_rooms > rooms)
+	if (new_cs.created == true)
 	{
-		cout << "Колличество работающих цехов не может превыщать колличество всего цехов на КС. Введите число работающих цехов:\n";
-		cin >> active_rooms;
+		int user_choice;
+		cout << "Вы точно хотите перезаписать КС?\n";
+		cout << "Выберете: <0>, если не хотите перезаписывать трубу,\n";
+		cout << "          <1>, если хотите перезаписать\n";
+		cin >> user_choice;
+		if (user_choice == 1)
+		{
+			int rooms, active_rooms, efficiency;
+			cout << "Укажите количество цехов: ";
+			cin >> rooms;
+			cout << "Укажите количество работающих цехов: ";
+			cin >> active_rooms;
+			if (active_rooms > rooms)
+			{
+				cout << "Колличество работающих цехов не может превыщать колличество всего цехов на КС. Введите число работающих цехов:\n";
+				cin >> active_rooms;
+			}
+			cout << "Введите значение эффективности: ";
+			cin >> efficiency;
+			new_cs.rooms = rooms;
+			new_cs.active_rooms = active_rooms;
+			new_cs.efficiency = efficiency;
+			new_cs.saved = false;
+			new_cs.created = true;
+		}
 	}
-	cout << "Введите значение эффективности: ";
-	cin >> efficiency;
-	new_cs.rooms = rooms;
-	new_cs.active_rooms = active_rooms;
-	new_cs.efficiency = efficiency;
-	new_cs.saved = false;
-	new_cs.created = true;
+	else
+	{
+		int rooms, active_rooms, efficiency;
+		cout << "Укажите количество цехов: ";
+		cin >> rooms;
+		cout << "Укажите количество работающих цехов: ";
+		cin >> active_rooms;
+		if (active_rooms > rooms)
+		{
+			cout << "Колличество работающих цехов не может превыщать колличество всего цехов на КС. Введите число работающих цехов:\n";
+			cin >> active_rooms;
+		}
+		cout << "Введите значение эффективности: ";
+		cin >> efficiency;
+		new_cs.rooms = rooms;
+		new_cs.active_rooms = active_rooms;
+		new_cs.efficiency = efficiency;
+		new_cs.saved = false;
+		new_cs.created = true;
+	}
 }
 
 void ViewAllObjects(tube& new_tube, cs& new_cs)
@@ -88,7 +154,7 @@ void ViewAllObjects(tube& new_tube, cs& new_cs)
 		cout << "Характеристики КС:\n";
 		cout << "Всего цехов: " << new_cs.rooms << endl;
 		cout << "Цехи в работе: " << new_cs.active_rooms << endl;
-		cout << "Эффективность: " << new_cs.efficiency << endl;
+		cout << "Эффективность: " << new_cs.efficiency << "\n\n";
 	}
 	else
 	{
@@ -101,18 +167,11 @@ void EditTube(tube& new_tube)
 	if (new_tube.created == true)
 	{
 		cout << "------------ Редактирование Трубы ------------\n";
-		double length, diameter;
-		int condition;
-		cout << "Введите новое значение длины:\n";
-		cin >> length;
-		cout << "Введите новое значение диаметра:\n";
-		cin >> diameter;
+		int user_choice;
 		cout << "Введите значение состояния:\n";
-		cout << "Труба исправна<1>   Труба в ремонте<2>\n";
-		cin >> condition;
-		new_tube.length = length;
-		new_tube.diameter = diameter;
-		if (condition == 1)
+		cout << "<0> - Труба исправна  <1> - Труба в ремонте\n";
+		cin >> user_choice;
+		if (user_choice == 0)
 		{
 			new_tube.condition = "Труба исправна";
 		}
@@ -132,22 +191,40 @@ void EditStation(cs& new_cs)
 {
 	if (new_cs.created == true)
 	{
-		int rooms, active_rooms, efficiency;
+		int user_choice = 3;
 		cout << "------------ Редактирование КС ------------\n";
-		cout << "Укажите количество цехов: ";
-		cin >> rooms;
-		cout << "Укажите количество работающих цехов: ";
-		cin >> active_rooms;
-		if (active_rooms > rooms)
+		cout << "Колличество работающих цехов на данный момент: " << new_cs.active_rooms << endl;
+		cout << "<0> - Убрать активный цех, <1> - Добавить активный цех, <2> - Закончить редактирование\n";
+		for (;;)
 		{
-			cout << "Колличество работающих цехов не может превышать колличество всего цехов на КС. Введите число работающих цехов:\n";
-			cin >> active_rooms;
+			cin >> user_choice;
+			if (user_choice == 0)
+			{
+				new_cs.active_rooms--;
+			}
+			if (user_choice == 1)
+			{
+				new_cs.active_rooms++;
+			}
+			if (user_choice == 2)
+			{
+				break;
+			}
+			if (new_cs.active_rooms > new_cs.rooms)
+			{
+				cout << "Колличество работающих цехов не может превышать колличество всего цехов на КС.\n";
+				cout << "Последние изменения не будут применены.\n\n";
+				new_cs.active_rooms--;
+				break;
+			}
+			if (new_cs.active_rooms < 0)
+			{
+				cout << "Колличество работающих цехов не может быть отрицательным.\n";
+				cout << "Последние изменения не будут применены.\n\n";
+				new_cs.active_rooms++;
+				break;
+			}
 		}
-		cout << "Введите значение эффективности: ";
-		cin >> efficiency;
-		new_cs.rooms = rooms;
-		new_cs.active_rooms = active_rooms;
-		new_cs.efficiency = efficiency;
 		new_cs.saved = false;
 	}
 	else
@@ -160,30 +237,10 @@ void Save(tube& new_tube, cs& new_cs)
 {
 	if (new_tube.saved == false)
 	{
-		int id = 0;
-		string current_word, line;
-		ifstream in("Tubes.txt");
-		if (in.is_open())
-		{
-			while (in >> current_word)
-			{
-				if (current_word == "Труба")
-				{
-					getline(in, line);
-					if (line != " исправна" and line != " в ремонте")
-					{
-						id++;
-					}
-				}
-			}
-		}
-		in.close();
-		id += 1;
-		ofstream out("Tubes.txt", ios::app);
+		ofstream out("Tubes.txt");
 		if (out.is_open())
 		{
 			out << "#####################" << endl;
-			out << "Труба номер: " << id << endl;
 			out << "Длина: " << new_tube.length << endl;
 			out << "Диаметр: " << new_tube.diameter << endl;
 			out << "Труба исправна" << endl;
@@ -193,26 +250,10 @@ void Save(tube& new_tube, cs& new_cs)
 	}
 	if (new_cs.saved == false)
 	{
-		int id = 0;
-		string current_word, line;
-		ifstream in("CS.txt");
-		if (in.is_open())
-		{
-			while (in >> current_word)
-			{
-				if (current_word == "Станция:")
-				{
-					id++;
-				}
-			}
-		}
-		in.close();
-		id += 1;
-		ofstream out("CS.txt", ios::app);
+		ofstream out("CS.txt");
 		if (out.is_open())
 		{
 			out << "#####################" << endl;
-			out << "Станция: " << id << endl;
 			out << "Всего цехов: " << new_cs.rooms << endl;
 			out << "Цехов в работе: " << new_cs.active_rooms << endl;
 			out << "Эффективность: " << new_cs.efficiency <<endl;
@@ -222,38 +263,81 @@ void Save(tube& new_tube, cs& new_cs)
 	}
 }
 
-void Download()
+void Download(tube& new_tube, cs& new_cs)
 {
-	cout << "--------------- Трубы ---------------\n";
-	string line;
+	cout << "--------------- Труба ---------------\n";
+	string line, current_word;
 	ifstream in("Tubes.txt");
 	if (in.is_open())
 	{
-		while (getline(in, line))
+		while (in>>current_word)
 		{
-			cout << line << endl;
+			if (current_word == "Длина:")
+			{
+				getline(in, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end()); //https://ru.stackoverflow.com/questions/1071460/%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D1%80%D0%BE%D0%B1%D0%B5%D0%BB%D0%BE%D0%B2-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8
+				new_tube.length = stod(line); //Преобразование строки в число https://arduinoplus.ru/preobrazovanie-stroki-string-v-czeloe-chislo-int-v-c/
+			}
+			if (current_word == "Диаметр:")
+			{
+				getline(in, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end());
+				new_tube.diameter = stod(line);
+			}
+			if (current_word == "Труба")
+			{
+				getline(in, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end());
+				new_tube.condition = "Труба "+ line;
+			}			
 		}
 	}
 	in.close();
-	cout << "--------------- КС ---------------\n";
+	new_tube.saved = true;
+	new_tube.created = true;
+	cout << "Длина: " << new_tube.length << endl;
+	cout << "Диаметр: " << new_tube.diameter << endl;
+	cout << "Состояние: " << new_tube.condition << endl;
+	cout << "---------------- КС -----------------\n";
 	ifstream inr("CS.txt");
 	if (inr.is_open())
 	{
-		while (getline(inr, line))
+		while (inr >> current_word)
 		{
-			cout << line << endl;
+			if (current_word == "цехов:")
+			{
+				getline(inr, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end()); //https://ru.stackoverflow.com/questions/1071460/%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D1%80%D0%BE%D0%B1%D0%B5%D0%BB%D0%BE%D0%B2-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8
+				new_cs.rooms = stoi(line); //Преобразование строки в число https://arduinoplus.ru/preobrazovanie-stroki-string-v-czeloe-chislo-int-v-c/
+			}
+			if (current_word == "работе:")
+			{
+				getline(inr, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end());
+				new_cs.active_rooms = stoi(line);
+			}
+			if (current_word == "Эффективность:")
+			{
+				getline(inr, line);
+				line.erase(remove(line.begin(), line.end(), ' '), line.end());
+				new_cs.efficiency = stoi(line);
+			}
 		}
 	}
 	inr.close();
+	new_cs.saved = true;
+	new_cs.created = true;
+	cout << "Всего цехов: " << new_cs.rooms << endl;
+	cout << "Цехов в работе: " << new_cs.active_rooms << endl;
+	cout << "Эффективность: " << new_cs.efficiency << "\n\n";
 }
 
 int Menu(tube& new_tube, cs& new_cs)
 {
-	int x;
-	setlocale(0, "");
+	int user_choice;
 	cout << "1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
-	cin >> x;
-	switch (x) 
+	cin >> user_choice;
+	switch (user_choice)
 	{
 		case 1:
 			system("cls");
@@ -287,7 +371,7 @@ int Menu(tube& new_tube, cs& new_cs)
 			break;
 		case 7:
 			system("cls");
-			Download();
+			Download(new_tube, new_cs);
 			return 1;
 			break;
 		case 0:
@@ -301,6 +385,7 @@ int Menu(tube& new_tube, cs& new_cs)
 
 int main()
 {
+	setlocale(0, "");
 	tube new_tube;
 	cs new_cs;
 	for (;;)
